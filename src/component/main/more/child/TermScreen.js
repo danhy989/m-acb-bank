c47cae40b0f770ae37523853362a0c43b26207cc
Duy Anh <anhvnd@innotech-vn.com>
2019-10-15 23:43:55 +0700

    term screen

--- src/component/main/more/child/TermScreen.js
+++ src/component/main/more/child/TermScreen.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Button from 'react-native-button';
 import { Text, View, Image, StyleSheet, ScrollView, TouchableNativeFeedback } from 'react-native';
+import Metrics from '../../../../common/Metrics';
 
 export default class TermScreen extends React.Component{
     render(){
+        const PADDING_HOZI = Metrics.PADDING_HORIZONTAL;
         return(
-            <View>
-                
+            <View style={{flex:1,padding:PADDING_HOZI,backgroundColor:'#F4F6F8'}}>
+                <Text style={{textAlign:'center',color:'#696A6B',fontSize:16,fontWeight:'bold',marginBottom:10}}>Điều kiện & điều khoản sử dụng dịch vụ Ngân hàng điện tử ACB online</Text>
+                <View style={{borderRadius:5,width:Metrics.screenWidth-PADDING_HOZI*2,height:50,backgroundColor:'#FFF'}}>
+
+                </View>
             </View>
         );
     }
